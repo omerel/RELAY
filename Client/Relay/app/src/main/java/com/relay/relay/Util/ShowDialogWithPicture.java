3e37766e17d6cb7d4c3b286555a8daafc9d9135e
Omer <elomer@gmail.com>
2017-04-11 00:12:08 +0300

    add zoom for image

--- Client/Relay/app/src/main/java/com/relay/relay/Util/ShowDialogWithPicture.java
+++ Client/Relay/app/src/main/java/com/relay/relay/Util/ShowDialogWithPicture.java
@@ -1,6 +1,7 @@
 package com.relay.relay.Util;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
@@ -24,24 +25,23 @@ public class ShowDialogWithPicture {
 
         View view = inflater.inflate(R.layout.dialog_show_picture, null);
 
-        ImageView imageView = (ImageView) view.findViewById(R.id.dialog_image);
+        TouchImageView imageView = (TouchImageView) view.findViewById(R.id.dialog_image);
 
         if (pic != null)
             imageView.setImageBitmap(pic);
+            imageView.setMaxZoom(4f);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
         builder.setView(view);
-
         // Add action buttons
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                // sign in the user ...
-            }
-        });
+//        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int id) {
+//                // sign in the user ...
+//            }
+//        });
 
         builder.create().show();
     }
